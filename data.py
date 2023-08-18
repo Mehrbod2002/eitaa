04c831412bb60b307c0a039ae303d8c2ce483b96
Mehrbod Akhlaghpour <m9.akhlaghpoor@gmail.com>
2023-08-18 06:25:33 +0330

    feat

--- data.py
+++ data.py
@@ -3,32 +3,17 @@ import time,argparse,sys,csv,os,asyncio
 
 js_code_send = '''
 (async function () {{
+    return new Promise(async (complete) => {{
     let mousedown = new MouseEvent("mousedown", {{
         bubbles: true,
         cancelable: true,
         view: window,
     }});
 
-    function clickElementAndWaitToFill(element, text) {{
-        return new Promise((resolve) => {{
-            let doneEvent = new Event('done');
-            let clickEvent = new Event('custom-click');
-
-            let handleDone = () => {{
-                element.removeEventListener('done', handleDone);
-                resolve();
-            }};
-
-            let handleCustomClick = () => {{
-                element.removeEventListener('custom-click', handleCustomClick);
-                element.dispatchEvent(doneEvent);
-            }};
-
-            element.addEventListener('done', handleDone);
-            element.addEventListener('custom-click', handleCustomClick);
+    async function clickElementAndWaitToFill(element, text) {{
+        return new Promise(async (resolve) => {{
             element.dispatchEvent(mousedown);
-
-            setTimeout(() => {{
+            setTimeout(async () => {{
                 let inputMessage = document.querySelector(".input-message-input.scrollable.scrollable-y.i18n.no-scrollbar");
                 inputMessage.textContent = text;
 
@@ -36,8 +21,9 @@ js_code_send = '''
                 inputMessage.dispatchEvent(inputEvent);
 
                 document.querySelector(".btn-icon.tgico-none.btn-circle.z-depth-1.btn-send.animated-button-icon.rp.send").click();
-                element.dispatchEvent(clickEvent);
-            }}, 1000);
+                await new Promise(resolve => setTimeout(resolve, 1500));
+                setTimeout(resolve, 300);
+            }}, 200);
         }});
     }}
     async function triggerCustomEvent() {{
@@ -59,40 +45,43 @@ js_code_send = '''
 
         await waitForClickCompletion();
 
-        let scrollingFinished = false;
-        let previous_chat_list = 0;
+        var previous_chat_list = 0;
+        var stop = false;
+        var i = 1;
         setTimeout(async () => {{
             var t = document.querySelectorAll(".chatlist.contacts-container")[0].querySelectorAll("li");
-            while (!scrollingFinished) {{
-                const chatList = document.querySelectorAll(".sidebar-content")[1].querySelector(".scrollable.scrollable-y");
-                chatList.scroll(0, document.body.scrollHeight + 10000);
+            while (!stop) {{
+                var chatList = document.querySelectorAll(".sidebar-content")[1].querySelector(".scrollable.scrollable-y");
+                chatList.scroll(0, document.body.scrollHeight + (i * 10000));
 
-                await new Promise((resolve) => setTimeout(resolve, 1000));
-
+                await new Promise(resolve => setTimeout(resolve, 200));
                 t = document.querySelectorAll(".chatlist.contacts-container")[0].querySelectorAll("li");
                 if (t.length == previous_chat_list) {{
-                    break;
+                    stop = true;
                 }}
+                i += 1;
                 previous_chat_list = t.length;
             }}
-            var i = 0;
+            var ii = 0;
             for (const element of t) {{
-                i += 1;
+                ii += 1;
                 await clickElementAndWaitToFill(element, "{message}");
             }}
-            console.log("Clicked on :",i);
             await triggerCustomEvent();
         }}, 1000);
     }};
 
     await processChatList();
+    await triggerCustomEvent();
+    setTimeout(complete,6000);
+    }});
 }})();
 '''
 
 async def send(number):
     try:
         async with async_playwright() as p:
-            browser = await p.chromium.launch(headless=True)
+            browser = await p.chromium.launch(headless=False)
             page = await browser.new_page()
 
             url = 'https://web.eitaa.com/'
@@ -121,7 +110,7 @@ async def send(number):
                     message_input = input("enter your message: ")
                     generated_code = js_code_send.format(message=message_input)
                     completed= await page.evaluate(generated_code)
-                    await page.wait_for_event('DONE', timeout=100000)
+                    await page.wait_for_event('DONE', timeout= 10**10*10*10)
                     if completed:
                         print("Done.")
                     else:
@@ -134,18 +123,18 @@ async def send(number):
         await browser.close()
 
 async def add(number):
-    file_path = input("User file path: ")
-    if os.path.exists(file_path) == False:
-        sys.exit("File not exist")
-    data = []
-    with open(file_path, 'r') as csvfile:
-        csvreader = csv.reader(csvfile)
-        for row in csvreader:
-            data.append(row)
-    if len(data[1:]) == 0:
-        sys.exit("Users list is null")
-    to_add = data[1:]
-    try:
+        file_path = input("User file path: ")
+        if os.path.exists(file_path) == False:
+            sys.exit("File not exist")
+        data = []
+        with open(file_path, 'r') as csvfile:
+            csvreader = csv.reader(csvfile)
+            for row in csvreader:
+                data.append(row)
+        if len(data[1:]) == 0:
+            sys.exit("Users list is null")
+        to_add = data[1:]
+    # try:
         async with async_playwright() as p:
             browser = await p.chromium.launch(headless=False)
             page = await browser.new_page()
@@ -158,7 +147,7 @@ async def add(number):
             input_elements = await page.query_selector_all('.input-field-input')
 
             if len(input_elements) >= 2:
-                    input_elements[1].type(number)
+                    await input_elements[1].type(number)
                     await page.wait_for_selector('.c-ripple')
                     await page.click('.btn-primary.btn-color-primary')
                     await page.wait_for_selector(".input-wrapper")
@@ -169,40 +158,52 @@ async def add(number):
                     input_element = await page.query_selector(input_selector)
 
                     if input_element:
-                        input_element.fill(get_code)
+                        await input_element.fill(get_code)
                     await page.wait_for_load_state('load')
                     await page.wait_for_selector(".whole.page-chats")
                     time.sleep(10)
-                    await page.query_selector('.btn-icon.btn-menu-toggle.rp.sidebar-tools-button.is-visible').click()
-                    await page.query_selector(".btn-menu-item.tgico-user.rp").click()
+                    data1 = await page.query_selector('.btn-icon.btn-menu-toggle.rp.sidebar-tools-button.is-visible')
+                    await data1.click()
+                    data_2= await page.query_selector(".btn-menu-item.tgico-user.rp")
+                    await data_2.click()
                     added = 0
                     for adder in to_add:
+                        adder[1] = "+"+adder[1]
                         number = 0
                         if len(adder[1]) == 13 and adder[1][0] == '+' and adder[1][0:4] == '+989':
                             number = adder[1][0:3] + " " + adder[1][3:6] + " " + adder[1][6:10] + " " + adder[1][10:13]
                         else:
                             continue
-                        await page.query_selector(".btn-circle.btn-corner.z-depth-1.is-visible.tgico-add.rp").click()
+                        data2 = await page.query_selector(".btn-circle.btn-corner.z-depth-1.is-visible.tgico-add.rp")
+                        await data2.click()
                         await page.wait_for_selector(".popup-header")
-                        await page.query_selector(".name-fields").query_selector(".input-field-input").fill(adder[0])
-                        await page.query_selector_all(".input-field.input-field-phone")[1].query_selector(".input-field-input").fill(number)
+                        data3 = await page.query_selector(".name-fields")
+                        data3 = await data3.query_selector(".input-field-input")
+                        await data3.fill(adder[0])
+                        data4 = await page.query_selector_all(".input-field.input-field-phone")
+                        data4 = await data4[1].query_selector(".input-field-input")
+                        data4 = await data4.fill(number)
                         await page.wait_for_timeout(100)
-                        if await page.query_selector(".popup-header").query_selector("button").is_disabled() == False:
-                            await page.query_selector(".popup-header").query_selector("button").click()
-                            await page.wait_for_function(
-                                '() => !document.querySelector(".popup-header").querySelector("button").classList.contains("disabled")',
-                            )
+                        data5 = await page.query_selector(".popup-header")
+                        data5 = await data5.query_selector("button")
+                        data5 = await data5.is_disabled()
+                        time.sleep(0.2)
+                        pop = await page.query_selector(".popup-header")
+                        pop = await pop.query_selector("button")
+                        if await pop.is_disabled() == False:
+                            await pop.click()
+                            time.sleep(1)
                             if await page.query_selector(".popup-header"):
-                                await page.query_selector(".btn-icon.popup-close.tgico-close").click()
+                                header1 = await page.query_selector(".btn-icon.popup-close.tgico-close")
+                                await header1.click()
                             else:
                                 added += 1
                     print(f"{added} numbers added .")
                     time.sleep(5)
-    except Exception as e:
-        print(f"Error: {e}")
-
-    finally:
-        await browser.close()
+    # except Exception as e:
+    #     print(f"Error: {e}")
+    # finally:
+    #     await browser.close()
 
 async def main():
     parser = argparse.ArgumentParser(description='Eitaa App')
@@ -217,7 +218,7 @@ async def main():
             if len(get_number) != 13 and get_number[0] != '+' and get_number[0:4] != '+989':
                 sys.exit("Invalid number")
             number = get_number[0:3] + " " + get_number[3:6] + " " + get_number[6:10] + " " + get_number[10:13]
-            add(number)
+            await add(number)
         elif selected_option == "send":
             get_number = input("Enter number: ").strip()
             if len(get_number) != 13 and get_number[0] != '+' and get_number[0:4] != '+989':
